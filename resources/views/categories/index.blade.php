38fd70b6457b560efe00651d3fd8e1d944b4c6f2
Trinanda Iman Karandi <sinjusaitama@gmail.com>
2026-09-07 11:16:18 +0700

    Implement basic MVC structure

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,2 @@
+<h1>Daftar categories</h1>
+<p>Sistem Informasi Perpustakaan</p>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoriesController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books', function() {
+    // return 'Ini daftar buku';
+    return view('books.index');
+});
+
+Route::get('/categories', function(){
+    return view('categories.index');
+});
+
+Route::get('/members', function() {
+    return view('members.index');
+});
+
+Route::get('/books', [BookController::class, 'index']);
