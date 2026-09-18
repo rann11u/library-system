a2a37b9e845c8037a8a77b3cb4c72740a6f20eea
Trinanda Iman Karandi <sinjusaitama@gmail.com>
2026-09-18 16:38:16 +0700

    Add books pages

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -7,6 +7,47 @@ use Illuminate\Http\Request;
 class BookController extends Controller
 {
     public function index() {
-        return view('books.index');
+        $title = "Daftar Buku";
+        $description = "Daftar buku yang tersedia di perpustakaan";
+        $books = [
+            'Pemrograman PHP',
+            'Laravel untuk Pemula',
+            'Basis Data',
+            'Algoritma dan Pemrograman',
+            'Pemrograman Berorientasi Objek',
+            'Habis gelap terbitlah terang',
+            'Atomic Habits',
+            'Madilog'
+        ]; 
+
+        $authors = [
+            'Budi Raharjo',
+            'Rian Ariona',
+            'Fathansyah',
+            'Rinaldi Munir',
+            'Rosa A.S.',
+            'R.A. Kartini',
+            'James Clear',
+            'Tan Malaka'
+        ];
+
+        $years = [
+            2018,
+            2021,
+            2015,
+            2016,
+            2019,
+            1911,
+            2018,
+            1943
+        ];
+
+        $stock = 8;
+
+        return view('books.index', compact('title', 'description', 'books', 'stock', 'authors', 'years'));
+    }
+
+    public function show($id){
+        return 'Detail Buku <br> ID Buku: ' . $id;
     }
 }
